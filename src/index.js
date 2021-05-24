0b3af03c91dfb4d393a8088b672c65aa07336af8
Koji <76919666+kafo8@users.noreply.github.com>
2021-05-24 18:20:07 -0400

    Fixed favicon loading

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import './index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const Index = () => {
-  return <div>Hello, BREW!</div>;
-};
+function Index() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 ReactDOM.render(<Index />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
         use: {
           loader: 'file-loader',
           options: {
-            name: '[path][name].[ext]',
+            name: '[name].[ext]',
           },
         },
       },
