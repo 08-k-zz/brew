a191f3453b20f0804cccf007bab01c11594a1661
Koji <cldoc507@gmail.com>
2021-05-23 22:15:21 -0400

    🐣 Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+const Index = () => {
+  return <div>Hello, BREW!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('root'));
