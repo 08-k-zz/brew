8c13c4e0f4c6f255d1786fb004d25a0406f1cd1a
Koji <76919666+kafo8@users.noreply.github.com>
2021-05-24 21:18:51 -0400

    Refactoring

--- server/app.js
+++ server/app.js
@@ -11,46 +11,35 @@ const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
 
 const app = express();
-const DIST_DIR = path.join(__dirname, '../dist');
-const HTML_FILE = path.join(DIST_DIR, 'index.html');
+const distDir = path.join(__dirname, '../dist');
+const htmlFile = path.join(distDir, 'index.html');
 
 if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
-app.use(express.static(DIST_DIR));
+app.use(express.static(distDir));
 app.get('/', (req, res) => {
-  res.sendFile(HTML_FILE);
+  res.sendFile(htmlFile);
 });
 
-// set security HTTP headers
 app.use(helmet());
 
-// parse json request body
 app.use(express.json());
-
-// parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-// sanitize request data
 app.use(xss());
 
-// gzip compression
 app.use(compression());
 
-// enable cors
 app.use(cors());
 app.options('*', cors());
 
-// send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
-// convert error to ApiError, if needed
 app.use(errorConverter);
-
-// handle error
 app.use(errorHandler);
 
 module.exports = app;
